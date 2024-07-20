57525b1d0e7ccc6852a8738581b9bd1d98b9c19f
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-07-20 22:44:52 +1000

    lets make drop a slash command

--- bot/discord/commands/staff/drop.js
+++ bot/discord/commands/staff/drop.js
@@ -1,106 +1,76 @@
 const ms = require("ms");
 const humanizeDuration = require("humanize-duration");
 
-exports.run = async (client, message, args) => {
-    if (!["569352110991343616", "1131236182899052696", "871722786006138960", "1080213687073251461"].includes(message.author.id)) return;
-    message.delete();
-
-    if (args[1] == null) {
-        message.reply("ayo fam, you need to specify a time.");
-        return;
-    }
-
-    const CAPSNUM = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    var codeGen = () => {
-        var password = "";
-        while (password.length < 16) {
-            password += CAPSNUM[Math.floor(Math.random() * CAPSNUM.length)];
+module.exports = {
+    async execute(interaction) {
+        const allowedUserIDs = ["569352110991343616", "1131236182899052696", "871722786006138960", "1080213687073251461"];
+        if (!allowedUserIDs.includes(interaction.user.id)) {
+            return interaction.reply({ content: "BACK OFF. STAFF ONLY.", ephemeral: true });
         }
-        return password;
-    };
 
-    const time = ms(args[1]) || 300000;
+        const timeString = interaction.options.getString('time');
+        const codeString = interaction.options.getString('code');
+        const time = ms(timeString) || 300000;
 
-    let code;
+        const CAPSNUM = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        const generateCode = () => {
+            let password = "";
+            while (password.length < 16) {
+                password += CAPSNUM[Math.floor(Math.random() * CAPSNUM.length)];
+            }
+            return password;
+        };
 
-    let moment = Date.now();
+        let code;
+        let now = Date.now();
 
-    if (!args[2]) {
-        const random = codeGen();
+        if (!codeString) {
+            const randomCode = generateCode();
+            code = {
+                code: randomCode,
+                createdBy: interaction.user.id,
+                balance: 1,
+                createdAt: now + time,
+            };
+            global.codes.set(randomCode, code);
+        } else {
+            code = global.codes.get(codeString);
+            if (!code) {
+                return interaction.reply({ content: "That's not a code you scammer", ephemeral: true });
+            }
+        }
 
-        code = codes.set(random, {
-            code: random,
-            createdBy: message.author.id,
-            balance: 1,
-            createdAt: moment + time,
+        const embed = new global.Discord.MessageEmbed()
+            .setAuthor("Key Drop!")
+            .setColor("BLUE")
+            .setFooter(`Keydrop by ${interaction.user.username}`, interaction.user.avatarURL())
+            .setDescription(`Dropping a premium key in: ${humanizeDuration(time, { round: true })}!`)
+            .setTimestamp(now + time);
+
+        const dropMessage = await interaction.reply({ embeds: [embed], fetchReply: true });
+
+        global.codes.set(`${code.code}.drop`, {
+            message: {
+                ID: dropMessage.id,
+                channel: dropMessage.channel.id,
+            },
         });
-    } else {
-        code = codes.get(args[2]);
-        if (code == null) {
-            message.reply("That's not a code you scammer");
-            return;
-        }
-    }
 
-    const embed = new Discord.MessageEmbed()
-        .setAuthor("Key Drop!")
-        .setColor("BLUE")
-        .setFooter(`Keydrop by ${message.author.username}`, bot.user.avatarURL)
-        .setDescription(
-            "Dropping a premium key in: " +
-                humanizeDuration(time, {
-                    round: true,
-                }) +
-                "!"
-        )
-        .setTimestamp(moment + time);
+        setTimeout(() => {
+            embed.setDescription(`Dropping a premium key in: ${humanizeDuration(time - time / 1.2, { round: true })}!`);
+            dropMessage.edit({ embeds: [embed] });
+        }, time / 1.2);
 
-    let msg = await message.reply("", {
-        embed: embed.setDescription(
-            "Dropping a premium key in: " +
-                humanizeDuration(time, {
-                    round: true,
-                }) +
-                "!"
-        ),
-    });
+        setTimeout(() => {
+            embed.setDescription(`Dropping a premium key in: ${humanizeDuration(time / 2, { round: true })}!`);
+            dropMessage.edit({ embeds: [embed] });
+        }, time / 2);
 
-    codes.set(code.code + ".drop", {
-        message: {
-            ID: msg.id,
-            channel: msg.channel.id,
-        },
-    });
-
-    setTimeout(() => {
-        msg.edit(
+        setTimeout(() => {
             embed.setDescription(
-                "Dropping a premium key in: " +
-                    humanizeDuration(time - time / 1.2, {
-                        round: true,
-                    }) +
-                    "!"
-            )
-        );
-    }, time / 1.2);
-
-    setTimeout(() => {
-        msg.edit(
-            embed.setDescription(
-                "Dropping a premium key in: " +
-                    humanizeDuration(time / 2, {
-                        round: true,
-                    }) +
-                    "!"
-            )
-        );
-    }, time / 2);
-
-    setTimeout(() => {
-        msg.edit(
-            embed.setDescription(
-                `**REDEEM NOW!**\nThe code is: \`${code.code}\` \n**Steps:** \n- Navigate to <#1250784987419246717>\n- Redeem the Premium Code: \`!server redeem <Code>\`\n\n*No one has redeemed the code yet!*`
-            )
-        );
-    }, time);
+                `**REDEEM NOW!**\nThe code is: \`${code.code}\` \n**Steps:** \n- Navigate to <#1250784987419246717>\n- Redeem the Premium Code: \`/server redeem <Code>\`\n\n*No one has redeemed the code yet!*`
+            );
+            dropMessage.edit({ embeds: [embed] });
+        }, time);
+    },
 };
