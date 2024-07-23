6e888c44bfd0451ac35db2963a61a63ce5b34629
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-07-23 19:14:38 +1000

    make update command a slash command

--- bot/discord/commands/staff/update.js
+++ bot/discord/commands/staff/update.js
@@ -1,25 +1,32 @@
-const cap = require("../../util/cap");
-const exec = require("child_process").exec;
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { exec } = require('child_process');
+const cap = require('../../util/cap');
 
-exports.run = async (client, message, args) => {
-    // Checks if the user has the Bot System Administrator Role
-    if (!message.member.roles.cache.find((r) => r.id === "1250361260462309430")) return console.log("No bot admin role");
-
-    exec(`git pull`, (error, stdout) => {
-        let response = error || stdout;
-        if (!error) {
-            if (response.includes("Already up to date.")) {
-                message.reply("All files are already up to date.");
-            } else {
-                client.channels.cache
-                    .get("1250044011457024040")
-                    .send(`<t:${Date.now().toString().slice(0, -3)}:f> Update requested by <@${message.author.id}>, pulling files.\n\`\`\`${cap(response, 1900)}\`\`\``);
-
-                message.reply("Pulling files from GitHub.");
-                setTimeout(() => {
-                    process.exit();
-                }, 1000);
-            }
+module.exports = { 
+    async execute(interaction) {
+        // Checks if the user has the Bot System Administrator Role
+        if (!interaction.member.roles.cache.some(r => r.id === '1250361260462309430')) {
+            return interaction.reply({ content: 'You do not have the necessary permissions to use this command.', ephemeral: true });
         }
-    });
+
+        exec('git pull', (error, stdout) => {
+            let response = error || stdout;
+            if (!error) {
+                if (response.includes('Already up to date.')) {
+                    return interaction.reply('All files are already up to date.');
+                } else {
+                    interaction.client.channels.cache
+                        .get('1256939961177997312')
+                        .send(`<t:${Math.floor(Date.now() / 1000)}:f> Update requested by <@${interaction.user.id}>, pulling files.\n\`\`\`${cap(response, 1900)}\`\`\``);
+
+                    interaction.reply('Pulling files from GitHub.');
+                    setTimeout(() => {
+                        process.exit();
+                    }, 1000);
+                }
+            } else {
+                interaction.reply(`An error occurred: \`\`\`${response}\`\`\``);
+            }
+        });
+    }
 };
