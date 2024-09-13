94b05227364c031a081492481c9938024f0c566f
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-09-13 11:10:12 +1000

    updates

---
+++ bot/discord/commands/user.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('User commands')
+            .addSubcommand(subcommand =>
+                subcommand
+                    .setName('new')
+                    .setDescription('Create a new account')
+                    .addStringOption(option => 
+                        option.setName('username')
+                            .setDescription('Your desired username (no spaces or special characters)')
+                            .setRequired(true))
+                    .addStringOption(option => 
+                        option.setName('email')
+                            .setDescription('Your valid email address')
+                            .setRequired(true))),
+    async execute(interaction) {
+        const subcommand = interaction.options.getSubcommand();
+        const subcommandPath = path.join(__dirname, 'user', `${subcommand}.js`);
+
+        if (fs.existsSync(subcommandPath)) {
+            const command = require(subcommandPath);
+            return command.execute(interaction);
+        }
+
+        await interaction.reply({ content: 'Subcommand not found', ephemeral: true });
+    },
+};
--- bot/discord/commands/user/new.js
+++ bot/discord/commands/user/new.js
@@ -1,226 +1,111 @@
-const axios = require("axios");
-const validator = require("validator");
-exports.run = async (client, message, args) => {
-    let getPassword = () => {
-        const CAPSNUM = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        var password = "";
-        while (password.length < 10) {
-            password += CAPSNUM[Math.floor(Math.random() * CAPSNUM.length)];
-        }
-        return password;
-    };
+const { EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+const validator = require('validator');
+const moment = require('moment');
 
-    if (userData.get(message.author.id) != null) {
-        message.reply("You already have a `panel account` linked to your discord account");
-        return;
-    }
+module.exports = {
+    async execute(interaction) {
+        const username = interaction.options.getString('username').toLowerCase();
+        const email = interaction.options.getString('email').toLowerCase();
 
-    let questions = [
-        {
-            id: "username",
-            question: "What should your username be? (**Please don't use spaces or special characters**)", // The questions...
-            filter: (m) => m.author.id === message.author.id, // Filter to use...
-            afterChecks: [
-                {
-                    check: (msg) => msg.trim().split(" ").length == 1,
-                    errorMessage: "username must not contain any spaces",
-                },
-            ],
-            time: 30000, // how much time a user has to answer the question before it times out
-            value: null, // The user's response.
-        },
-        {
-            id: "email",
-            question: "What's your email? *(must be a valid email)*",
-            filter: (m) => m.author.id === message.author.id,
-            afterChecks: [
-                {
-                    check: (msg) => validator.isEmail(msg.toLowerCase().trim()),
-                    errorMessage: "The email must be valid.",
-                },
-            ],
-            time: 30000,
-            value: null,
-        },
-    ];
-
-    // Locate the account creation category
-    let category = message.guild.channels.cache.find((c) => c.id === "1250359312249917501" && c.type === "category");
-
-    // if not found throw an error
-    if (!category) throw new Error("Category channel does not exist");
-
-    // Create the channel in which the user will use to create his account
-    let channel = await message.guild.channels
-        .create(message.author.tag, {
-            parent: category.id,
-            permissionOverwrites: [
-                {
-                    id: message.author.id,
-                    allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"],
-                },
-                {
-                    id: message.guild.id,
-                    deny: 0x400,
-                },
-            ],
-        })
-        .catch(console.error);
-
-    channel.updateOverwrite(message.author, {
-        VIEW_CHANNEL: true,
-        SEND_MESSAGES: true,
-        READ_MESSAGE_HISTORY: true,
-    });
-
-    // Tell the user to check the channel.
-    message.reply(`Please check <#${channel.id}> to create an account.`);
-
-    //Send the initial question.
-
-    let msg = null;
-
-    for (const question of questions) {
-        if (msg == null) {
-            msg = await channel.send(message.member, {
-                embed: new Discord.MessageEmbed()
-                    .setColor(0x36393e)
-                    .setDescription(question.question)
-                    .setFooter("You can type 'cancel' to cancel the request"),
-            });
-        } else {
-            msg.edit(message.member, {
-                embed: msg.embeds[0].setDescription(question.question),
-            });
-        }
-
-        let awaitMessages = await channel
-            .awaitMessages(question.filter, {
-                max: 1,
-                time: question.time,
-                errors: ["time"],
-            })
-            .catch((x) => {
-                channel.send("User failed to provide an input!\nAccount creation cancelled!");
-                setTimeout(() => {
-                    channel.delete();
-                }, 5000);
-                return;
-            });
-        // Log the value...
-        question.value = awaitMessages.first().content.trim();
-
-        await awaitMessages.first().delete();
-
-        if (question.value == "cancel") {
-            msg.delete();
-            channel.send("Cancelled!");
-
-            setTimeout(() => {
-                channel.delete();
-            }, 5000);
+        if (userData.get(interaction.user.id)) {
+            await interaction.reply({ content: "You already have a `panel account` linked to your Discord account.", ephemeral: true });
             return;
         }
 
-        for (const aftercheck of question.afterChecks) {
-            if (aftercheck.check(question.value) == false) {
-                channel.send(aftercheck.errorMessage);
-                channel.send("Account creation cancelled!");
-                setTimeout(() => {
-                    channel.delete();
-                }, 5000);
-                return;
-            }
+        if (username.includes(" ") || /[^a-zA-Z0-9]/.test(username)) {
+            await interaction.reply({ content: "Username must not contain spaces or special characters.", ephemeral: true });
+            return;
         }
-    }
 
-    msg.edit(message.member, {
-        embed: msg.embeds[0]
-            .setDescription(
-                "Attempting to create an account for you...\n\n>>> " +
-                    questions.map((question) => `**${question.id}:** ${question.value.toLowerCase()}`).join("\n")
-            )
-            .setFooter("")
-            .setTimestamp(),
-    });
+        if (!validator.isEmail(email)) {
+            await interaction.reply({ content: "Please provide a valid email address.", ephemeral: true });
+            return;
+        }
 
-    const data = {
-        username: questions.find((question) => question.id == "username").value.toLowerCase(),
-        email: questions.find((question) => question.id == "email").value.toLowerCase(),
-        first_name: questions.find((question) => question.id == "username").value,
-        last_name: ".",
-        password: getPassword(),
-        root_admin: false,
-        language: "en",
-    };
+        const password = generatePassword();
 
-    axios({
-        url: config.Pterodactyl.hosturl + "/api/application/users",
-        method: "POST",
-        followRedirect: true,
-        maxRedirects: 5,
-        headers: {
-            Authorization: "Bearer " + config.Pterodactyl.apikey,
-            "Content-Type": "application/json",
-            Accept: "Application/vnd.pterodactyl.v1+json",
-        },
-        data: data,
-    })
-        .then((user) => {
-            userData.set(`${message.author.id}`, {
-                discordID: message.author.id,
-                consoleID: user.data.attributes.id,
-                email: user.data.attributes.email,
-                username: user.data.attributes.username,
-                linkTime: moment().format("HH:mm:ss"),
-                linkDate: moment().format("YYYY-MM-DD"),
+        const accountData = {
+            username,
+            email,
+            first_name: username,
+            last_name: ".",
+            password,
+            root_admin: false,
+            language: "en",
+        };
+
+        try {
+            const response = await axios.post(
+                `${config.Pterodactyl.hosturl}/api/application/users`,
+                accountData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${config.Pterodactyl.apikey}`,
+                        'Content-Type': 'application/json',
+                    }
+                }
+            );
+
+            userData.set(interaction.user.id, {
+                discordID: interaction.user.id,
+                consoleID: response.data.attributes.id,
+                email: response.data.attributes.email,
+                username: response.data.attributes.username,
+                linkTime: moment().format('HH:mm:ss'),
+                linkDate: moment().format('YYYY-MM-DD'),
                 domains: [],
             });
 
-            msg.edit("Hello! You created an new account, Here's the login information", {
-                embed: new Discord.MessageEmbed()
-                    .setColor("GREEN")
-                    .setDescription(
-                        "URL: " +
-                            config.Pterodactyl.hosturl +
-                            " \nUsername: " +
-                            data.username +
-                            " \nEmail: " +
-                            data.email +
-                            " \nPassword: " +
-                            data.password
-                    )
-                    .setFooter("Please note: It is recommended that you change the password"),
-            });
+            const userEmbed = new EmbedBuilder()
+                .setColor('Green')
+                .setTitle('Account Created')
+                .setDescription('Here is your account information:')
+                .addFields(
+                    { name: 'Username', value: username, inline: true },
+                    { name: 'Email', value: email, inline: true },
+                    { name: 'Password', value: password, inline: true },
+                    { name: 'URL', value: config.Pterodactyl.hosturl }
+                )
+                .setFooter({ text: 'Please change your password after logging in.' });
 
-            channel.send("**You have 30 minutes to keep note of this info before the channel is deleted.**");
-            message.guild.members.cache.get(message.author.id).roles.add("1250744275566592033");
-            setTimeout(function () {
-                channel.delete();
-            }, 1800000);
-        })
-        .catch((err) => {
-            let errors = err.response.data.errors;
+            await interaction.reply({ embeds: [userEmbed], ephemeral: true });
 
-            if (errors) {
-                msg.edit("", {
-                    embed: new Discord.MessageEmbed()
-                        .setColor("RED")
-                        .setTitle("An error has occured:")
-                        .setDescription(
-                            "**ERRORS:**\n\n- " + errors.map((error) => error.detail.replace("\n", " ")).join("\n- ")
-                        )
-                        .setTimestamp()
-                        .setFooter("Deleting in 30 seconds..."),
-                });
-                setTimeout(function () {
-                    channel.delete();
-                }, 30000);
+            const logEmbed = new EmbedBuilder()
+                .setColor('Blue')
+                .setTitle('New Account Created')
+                .addFields(
+                    { name: 'Discord Account', value: `<@${interaction.user.id}> (${interaction.user.id})`, inline: true },
+                    { name: 'Console Account Email', value: response.data.attributes.email, inline: true },
+                    { name: 'Created At', value: `${moment().format('HH:mm:ss')} / ${moment().format('YYYY-MM-DD')}`, inline: true },
+                    { name: 'Console Username', value: response.data.attributes.username, inline: true },
+                    { name: 'Console ID', value: response.data.attributes.id.toString(), inline: true }
+                )
+                .setTimestamp()
+                .setFooter({ text: 'Account created successfully.' });
+
+            const logChannel = interaction.client.channels.cache.get('1250044501180026881');
+            if (logChannel) {
+                await logChannel.send({ embeds: [logEmbed] });
             } else {
-                channel.send("An unexpected error has occured, please try again later...");
-                setTimeout(function () {
-                    channel.delete();
-                }, 30000);
+                console.error('Log channel not found');
             }
-        });
+
+        } catch (error) {
+            console.error('Error creating account:', error);
+            await interaction.reply({
+                content: `There was an error creating your account. Please try again later.`,
+                ephemeral: true,
+            });
+        }
+    }
 };
+
+function generatePassword() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    let password = '';
+    for (let i = 0; i < 10; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return password;
+}
