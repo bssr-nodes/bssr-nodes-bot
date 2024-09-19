d503185703c8622f28987def34c4d4a0cb115f99
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-09-19 18:37:44 +1000

    fix: define client

--- bot/discord/util/registercommands.js
+++ bot/discord/util/registercommands.js
@@ -1,52 +1,50 @@
-// DO NOT USE UNLESS YOURE USING IT ON THE MAIN BOT!!! IT WILL NOT WORK IF YOU ARE NOT USING IT ON PTERODACTYL PANEL.
-
 const fs = require('fs');
 const path = require('path');
 
-client.commands = new Map();
-const commands = [];
+module.exports = async (client) => {
+    client.commands = new Map();
+    const commands = [];
 
-function loadCommands(directory) {
-    const entries = fs.readdirSync(directory, { withFileTypes: true });
+    function loadCommands(directory) {
+        const entries = fs.readdirSync(directory, { withFileTypes: true });
 
-    for (const entry of entries) {
-        const fullPath = path.join(directory, entry.name);
+        for (const entry of entries) {
+            const fullPath = path.join(directory, entry.name);
 
-        if (entry.isDirectory()) {
-            loadCommands(fullPath);
-        } else if (entry.isFile() && entry.name.endsWith('.js')) {
-            try {
-                const command = require(fullPath);
+            if (entry.isDirectory()) {
+                loadCommands(fullPath);
+            } else if (entry.isFile() && entry.name.endsWith('.js')) {
+                try {
+                    const command = require(fullPath);
 
-                if (command.data && command.data.name) {
-                    client.commands.set(command.data.name, command);
-                    commands.push(command.data.toJSON());
+                    if (command.data && command.data.name) {
+                        client.commands.set(command.data.name, command);
+                        commands.push(command.data.toJSON());
+                    }
+                } catch (error) {
+                    console.error(`Failed to load command ${entry.name}:`, error);
                 }
-            } catch (error) {
-                console.error(`Failed to load command ${entry.name}:`, error);
             }
         }
     }
-}
 
-(async () => {
     try {
         const guildId = '1244976723800358994';
         const guild = client.guilds.cache.get(guildId);
 
         const globalCommands = await client.application.commands.fetch();
-        globalCommands.forEach(command => {
-            client.application.commands.delete(command.id)
+        for (const command of globalCommands.values()) {
+            await client.application.commands.delete(command.id)
                 .then(() => console.log(`Deleted global command: ${command.name}`))
                 .catch(console.error);
-        });
+        }
 
         const guildCommands = await guild.commands.fetch();
-        guildCommands.forEach(command => {
-            guild.commands.delete(command.id)
+        for (const command of guildCommands.values()) {
+            await guild.commands.delete(command.id)
                 .then(() => console.log(`Deleted guild command: ${command.name}`))
                 .catch(console.error);
-        });
+        }
 
         const baseDirectory = path.resolve('/home/container/bot/discord/commands');
         loadCommands(baseDirectory);
@@ -57,4 +55,4 @@ function loadCommands(directory) {
     } catch (error) {
         console.error('Error deleting/re-registering commands:', error);
     }
-})();
+};
