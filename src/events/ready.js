d773ce7421d8529ad425be517fd19cf64ce90a87
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2025-02-20 19:32:53 +1100

    updates

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,7 +3,8 @@ const Chalk = require("chalk");
 const { exec } = require("child_process");
 const Util = require('util');
 const execPromise = Util.promisify(exec);
-
+const axios = require('axios');
+const { transferServer } = require('./transfer.js');
 
 const ServerStatus = require("../serverStatus.js");
 const Config = require('../../config.json');
@@ -19,15 +20,12 @@ module.exports = async (client) => {
         Chalk.magenta("[DISCORD] ") + Chalk.green(client.user.username + " has logged in!"),
     );
 
-    // Cloes all accounts channels that are older than 30 minutes.
     guild.channels.cache
     .filter((x) => x.parentId === MiscConfigs.accounts && Date.now() - x.createdAt.getTime() > 30 * 60 * 1000)
     .forEach((x) => x.delete());
 
-    //Initializing the cooldown.
     client.cooldown = {};
 
-    //Automatic GitHub Update (30 seconds intervals).
     setInterval(async () => {
         try {
             const { stdout } = await execPromise('git pull');
@@ -43,9 +41,10 @@ module.exports = async (client) => {
                 }, 5000);
             }
         } catch (Error) {
+            console.error("Error during Git pull:", Error);
         }
     }, 30 * 1000);
-    
+
     setInterval(() => {
         client.user.setPresence({
             activities: [{ name: 'over BSSR Nodes', type: Discord.ActivityType.Watching }],
@@ -54,21 +53,14 @@ module.exports = async (client) => {
     }, 1000 * 60);
 
     if (true) {
-
         console.log(Chalk.magenta("[NODE CHECKER] ") + Chalk.greenBright("Enabled"));
+        await ServerStatus.startNodeChecker();
 
-        await ServerStatus.startNodeChecker(); //Start the Node Checker.
-
-        // Node Status Embed.
         const channel = client.channels.cache.get(MiscConfigs.nodestatus);
-
         setInterval(async () => {
             const embed = await ServerStatus.getEmbed();
 
-            let messages = await channel.messages.fetch({
-                limit: 10,
-            });
-
+            let messages = await channel.messages.fetch({ limit: 10 });
             messages = messages.filter((x) => x.author.id === client.user.id).last();
 
             if (messages == null) channel.send({embeds: [embed]});
@@ -77,4 +69,39 @@ module.exports = async (client) => {
     } else {
         console.log(Chalk.magenta("[NODE CHECKER] ") + Chalk.redBright("Disabled"));
     }
+
+    setInterval(async () => {
+        try {
+            const { data } = await axios.get(`${Config.Pterodactyl.hosturl}/api/application/servers`, {
+                headers: {
+                    Authorization: `Bearer ${Config.Pterodactyl.apikey}`,
+                    'Accept': 'Application/vnd.pterodactyl.v1+json',
+                },
+            });
+
+            const testServerID = '05a48774';
+
+            data.data.forEach(async (server) => {
+                const serverID = server.attributes.id;
+                const nodeID = server.attributes.node;
+
+                if (serverID !== testServerID) return;
+
+                console.log(`Checking server ${serverID}...`);
+
+                if (nodeID === '2') {
+                    const user = await userData.get({ where: { consoleID: serverID } });
+
+                    if (user && !user.isWhitelisted) {
+                        console.log(`Server ${serverID} is on the private node and the user is not whitelisted, initiating transfer.`);
+                        await transferServer(client, serverID);
+                    } else {
+                        console.log(`Server ${serverID} is on the private node, but the user is whitelisted.`);
+                    }
+                }
+            });
+        } catch (error) {
+            console.error("Error fetching servers from Pterodactyl API:", error);
+        }
+    }, 30 * 60 * 1000); // Every 30 minutes
 };
