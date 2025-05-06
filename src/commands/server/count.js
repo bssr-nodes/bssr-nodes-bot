2c56c01113d3676738a3b02d346c06d2d3f84a88
blxddy <95270582+blxddy@users.noreply.github.com>
2025-05-06 22:22:03 +1000

    attempt yet again another fix

--- src/commands/server/count.js
+++ src/commands/server/count.js
@@ -15,31 +15,36 @@ exports.description = "Shows the amount of servers a user has.";
 exports.run = async (client, message, args) => {
 
     const UserId =
-    args[1] && args[1].match(/[0-9]{17,19}/)
-        ? args[1].match(/[0-9]{17,19}/)[0]
-        : message.author.id;
+        args[1] && args[1].match(/[0-9]{17,19}/)
+            ? args[1].match(/[0-9]{17,19}/)[0]
+            : message.author.id;
 
     const userAccount = await userData.get(UserId);
 
-    if (userAccount == null) return message.channel.send('User does not have account linked.');
+    if (!userAccount) {
+        return message.channel.send('User does not have an account linked.');
+    }
 
-    await getUserServers(userAccount.consoleID).then(async (Response) => {
-            const userServers = Response.attributes.relationships.servers.data; //The user server data from the panel.
-            const serversUsed = userServers.length;
-            const serverLimit = ParseInt(userAccount.serverLimit, 10) || 3;
-            const remainingSlots = Math.max(serversUsed - serverLimit, 0);
+    try {
+        const response = await getUserServers(userAccount.consoleID);
+        const userServers = response.attributes.relationships.servers.data; // The user server data from the panel.
+        const serversUsed = userServers.length;
+        const serverLimit = parseInt(userAccount.serverLimit, 10) || 3;
+        const remainingSlots = Math.max(serverLimit - serversUsed, 0);
 
-            const serverCountEmbed = new Discord.EmbedBuilder()
-                .setTitle(`Server Count:`)
-                .setDescription(`
-            Servers used: ${serversUsed} out of ${serverLimit.toLocaleString()}.\nRemaining slots: ${remainingSlots}.
-                    `)
-                .setColor("Blurple")
-                .setFooter({text: `Requested by ${message.author.tag}`, iconURL: message.author.displayAvatarURL()})
-                .setTimestamp();
+        const serverCountEmbed = new Discord.EmbedBuilder()
+            .setTitle(`Server Count:`)
+            .setDescription(`
+                Servers used: ${serversUsed} out of ${serverLimit.toLocaleString()}.\n
+                Remaining slots: ${remainingSlots}.
+            `)
+            .setColor("Blurple")
+            .setFooter({ text: `Requested by ${message.author.tag}`, iconURL: message.author.displayAvatarURL() })
+            .setTimestamp();
 
-            await message.reply({embeds: [serverCountEmbed]});
-    }).catch(async (Error) => {
+        await message.reply({ embeds: [serverCountEmbed] });
+    } catch (error) {
+        console.error(error);
         await message.reply("An error occurred while loading servers.");
-    });
+    }
 };
