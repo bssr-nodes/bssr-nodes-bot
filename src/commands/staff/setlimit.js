98675839cba889c1182cf24f2a61e1fd5a7a3bbb
blxddy <95270582+blxddy@users.noreply.github.com>
2025-05-26 21:38:35 +1000

    fix setlimit cmd

--- src/commands/staff/setlimit.js
+++ src/commands/staff/setlimit.js
@@ -13,18 +13,17 @@ exports.description = "Adjust the server limit for a user.";
 exports.run = async (client, message, args) => {
     if (!message.member.roles.cache.some(role => Config.DiscordBot.Roles.Admin.includes(role.id))) return;
 
-    if (args.length < 5) {
-        return message.reply("Usage: `!staff setlimit <user_id> <new_limit>`");
-    }
+    if (args.length < 3) { return message.reply("Usage: `!staff setlimit <user_id|> <new_limit>`"); }
 
-    const userId = args[1];
+    let userId;
+    const mention = message.mentions.users.first();
+    if (mention) { userId = mention.id;} else {userId = args[1];}
+
     const newLimit = parseInt(args[2]);
+    if (isNaN(newLimit) || newLimit < 1) {return message.reply("Please provide a valid number greater than 0 for the new limit.");}
 
-    if (isNaN(newLimit) || newLimit < 1) {
-        return message.reply("Please provide a valid number greater than 0 for the new limit.");
-    }
-
-    const userDataEntry = await userData.get(userId) || { serverLimit: 5 };
+    const userDataEntry = await userData.get(userId);
+    if (!userDataEntry) {return message.reply("That user does not have an account in the system.");}
 
     userDataEntry.serverLimit = newLimit;
     await userData.set(userId, userDataEntry);
